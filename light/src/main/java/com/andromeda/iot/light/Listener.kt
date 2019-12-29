2f5a2894569ae37d3d8aa0d1108ba6c38f00fdef
fulton <gprlabs@outlook.com>
2019-12-28 17:42:34 -0800

    will this not work

--- light/src/main/java/com/andromeda/iot/light/Listener.kt
+++ light/src/main/java/com/andromeda/iot/light/Listener.kt
@@ -4,7 +4,7 @@ import com.microsoft.azure.sdk.iot.device.IotHubMessageResult
 import com.microsoft.azure.sdk.iot.device.Message
 
 class Listener : com.microsoft.azure.sdk.iot.device.MessageCallback {
-    override fun execute(message: Message?, callbackContext: java.lang.Object?): IotHubMessageResult {
+    override fun execute(message: Message?, callbackContext: Object?): IotHubMessageResult {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
--- light/src/main/java/com/andromeda/iot/light/ReadData.java
+++ light/src/main/java/com/andromeda/iot/light/ReadData.java
@@ -1,5 +1,4 @@
 package com.andromeda.iot.light;
-
 public class ReadData {
 
 }
